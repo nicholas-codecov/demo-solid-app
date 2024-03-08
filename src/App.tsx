76b30234786410d5561a0359ce01ffb318185acb
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-03-08 15:28:04 -0400

    add a ton of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
 
   return (
     <>
@@ -21,6 +21,30 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
         </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +53,7 @@ function App() {
         Click on the Vite and Solid logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
